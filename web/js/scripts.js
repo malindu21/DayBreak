9fb934472afccd1674f4dd7020a4d7248631b9d4
Malindu Chathuranga <53685082+malindu21@users.noreply.github.com>
2023-11-25 21:16:12 +0530

    IP

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -307,15 +307,16 @@
     });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+    $(function() {
+        $(document).on('click', 'a.page-scroll', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 100
+            }, 100, 'easeInOutExpo'); // Reduced duration to 300 milliseconds
+            event.preventDefault();
+        });
+    });
+    
 
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
